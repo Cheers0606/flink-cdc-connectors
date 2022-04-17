6a34c9d9d314d3f0c6e3abf186a9183797ac35d8
莫贤彬 <moxianbin526976203@gmail.com>
2022-04-17 21:23:14 +0800

    [oracle]Use Incremental Snapshot Framework for Oracle CDC Connector
    [oracle]Use Incremental Snapshot Framework for Oracle CDC Connector

---
+++ flink-connector-oracle-cdc/src/main/java/com/ververica/cdc/connectors/oracle/source/config/OracleSourceConfigFactory.java
@@ -0,0 +1,89 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ververica.cdc.connectors.oracle.source.config;
+
+import com.ververica.cdc.connectors.base.config.JdbcSourceConfigFactory;
+import io.debezium.config.Configuration;
+import io.debezium.connector.oracle.OracleConnector;
+
+import java.util.Properties;
+
+import static org.apache.flink.util.Preconditions.checkNotNull;
+
+/** A factory to initialize {@link OracleSourceConfig}. */
+public class OracleSourceConfigFactory extends JdbcSourceConfigFactory {
+
+    private static final String DATABASE_SERVER_NAME = "oracle_logminer";
+
+    /** Creates a new {@link OracleSourceConfig} for the given subtask {@code subtaskId}. */
+    public OracleSourceConfig create(int subtaskId) {
+        Properties props = new Properties();
+        props.setProperty("connector.class", OracleConnector.class.getCanonicalName());
+        // Logical name that identifies and provides a namespace for the particular Oracle
+        // database server being
+        // monitored. The logical name should be unique across all other connectors, since it is
+        // used as a prefix
+        // for all Kafka topic names emanating from this connector. Only alphanumeric characters
+        // and
+        // underscores should be used.
+        props.setProperty("database.server.name", DATABASE_SERVER_NAME);
+        props.setProperty("database.hostname", checkNotNull(hostname));
+        props.setProperty("database.user", checkNotNull(username));
+        props.setProperty("database.password", checkNotNull(password));
+        props.setProperty("database.port", String.valueOf(port));
+        props.setProperty("database.history.skip.unparseable.ddl", String.valueOf(true));
+        props.setProperty("database.dbname", checkNotNull(databaseList.get(0)));
+
+        if (databaseList != null) {
+            props.setProperty("schema.include.list", String.join(",", databaseList));
+        }
+        if (tableList != null) {
+            props.setProperty("table.include.list", String.join(",", tableList));
+        }
+
+        // override the user-defined debezium properties
+        if (dbzProperties != null) {
+            props.putAll(dbzProperties);
+        }
+
+        Configuration dbzConfiguration = Configuration.from(props);
+        String driverClassName = OracleConnector.class.getCanonicalName();
+        return new OracleSourceConfig(
+                startupOptions,
+                databaseList,
+                tableList,
+                splitSize,
+                splitMetaGroupSize,
+                distributionFactorUpper,
+                distributionFactorLower,
+                includeSchemaChanges,
+                props,
+                dbzConfiguration,
+                driverClassName,
+                hostname,
+                port,
+                username,
+                password,
+                fetchSize,
+                serverTimeZone,
+                connectTimeout,
+                connectMaxRetries,
+                connectionPoolSize);
+    }
+}
---
+++ flink-connector-oracle-cdc/src/main/java/com/ververica/cdc/connectors/oracle/source/reader/fetch/OracleStreamFetchTask.java
@@ -0,0 +1,163 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ververica.cdc.connectors.oracle.source.reader.fetch;
+
+import com.ververica.cdc.connectors.base.relational.JdbcSourceEventDispatcher;
+import com.ververica.cdc.connectors.base.source.meta.split.SourceSplitBase;
+import com.ververica.cdc.connectors.base.source.meta.split.StreamSplit;
+import com.ververica.cdc.connectors.base.source.reader.external.FetchTask;
+import com.ververica.cdc.connectors.oracle.source.meta.offset.RedoLogOffset;
+import io.debezium.config.Configuration;
+import io.debezium.connector.oracle.OracleConnection;
+import io.debezium.connector.oracle.OracleConnectorConfig;
+import io.debezium.connector.oracle.OracleDatabaseSchema;
+import io.debezium.connector.oracle.OracleOffsetContext;
+import io.debezium.connector.oracle.OracleStreamingChangeEventSourceMetrics;
+import io.debezium.connector.oracle.OracleTaskContext;
+import io.debezium.connector.oracle.logminer.LogMinerStreamingChangeEventSource;
+import io.debezium.pipeline.ErrorHandler;
+import io.debezium.pipeline.source.spi.ChangeEventSource;
+import io.debezium.util.Clock;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.ververica.cdc.connectors.oracle.source.meta.offset.RedoLogOffset.NO_STOPPING_OFFSET;
+
+/** The task to work for fetching data of Oracle table stream split. */
+public class OracleStreamFetchTask implements FetchTask<SourceSplitBase> {
+
+    private final StreamSplit split;
+    private volatile boolean taskRunning = false;
+    private OracleRedoLogSplitReadTask redoLogSplitReadTask;
+
+    public OracleStreamFetchTask(StreamSplit split) {
+        this.split = split;
+    }
+
+    @Override
+    public void execute(Context context) throws Exception {
+        OracleSourceFetchTaskContext sourceFetchContext = (OracleSourceFetchTaskContext) context;
+        taskRunning = true;
+        redoLogSplitReadTask =
+                new OracleRedoLogSplitReadTask(
+                        sourceFetchContext.getDbzConnectorConfig(),
+                        sourceFetchContext.getOffsetContext(),
+                        sourceFetchContext.getConnection(),
+                        sourceFetchContext.getDispatcher(),
+                        sourceFetchContext.getErrorHandler(),
+                        sourceFetchContext.getDatabaseSchema(),
+                        sourceFetchContext.getSourceConfig().getDbzConnectorConfig().jdbcConfig(),
+                        sourceFetchContext.getTaskContext(),
+                        sourceFetchContext.getStreamingChangeEventSourceMetrics(),
+                        split);
+        BinlogSplitChangeEventSourceContext changeEventSourceContext =
+                new BinlogSplitChangeEventSourceContext();
+        redoLogSplitReadTask.execute(changeEventSourceContext);
+    }
+
+    @Override
+    public boolean isRunning() {
+        return taskRunning;
+    }
+
+    @Override
+    public StreamSplit getSplit() {
+        return split;
+    }
+
+    /**
+     * A wrapped task to read all binlog for table and also supports read bounded (from lowWatermark
+     * to highWatermark) binlog.
+     */
+    public static class OracleRedoLogSplitReadTask extends LogMinerStreamingChangeEventSource {
+
+        private static final Logger LOG = LoggerFactory.getLogger(OracleRedoLogSplitReadTask.class);
+        private final StreamSplit redoLogSplit;
+        private final OracleOffsetContext offsetContext;
+        private final JdbcSourceEventDispatcher dispatcher;
+        private final ErrorHandler errorHandler;
+        private ChangeEventSourceContext context;
+
+        public OracleRedoLogSplitReadTask(
+                OracleConnectorConfig connectorConfig,
+                OracleOffsetContext offsetContext,
+                OracleConnection connection,
+                JdbcSourceEventDispatcher dispatcher,
+                ErrorHandler errorHandler,
+                OracleDatabaseSchema schema,
+                Configuration jdbcConfig,
+                OracleTaskContext taskContext,
+                OracleStreamingChangeEventSourceMetrics metrics,
+                StreamSplit redoLogSplit) {
+            super(
+                    connectorConfig,
+                    offsetContext,
+                    connection,
+                    dispatcher,
+                    errorHandler,
+                    Clock.SYSTEM,
+                    schema,
+                    taskContext,
+                    jdbcConfig,
+                    metrics);
+            this.redoLogSplit = redoLogSplit;
+            this.dispatcher = dispatcher;
+            this.offsetContext = offsetContext;
+            this.errorHandler = errorHandler;
+        }
+
+        @Override
+        public void execute(ChangeEventSourceContext context) {
+            this.context = context;
+            super.execute(context);
+        }
+
+        // TODO: how to stop for fetch binlog for snapshot split? we can reimplements
+        // StreamingChangeEventSource that copy from LogMinerStreamingChangeEventSource and add
+        // event handle method
+
+        private boolean isBoundedRead() {
+            return !NO_STOPPING_OFFSET.equals(redoLogSplit.getEndingOffset());
+        }
+
+        public static RedoLogOffset getBinlogPosition(Map<String, ?> offset) {
+            Map<String, String> offsetStrMap = new HashMap<>();
+            for (Map.Entry<String, ?> entry : offset.entrySet()) {
+                offsetStrMap.put(
+                        entry.getKey(),
+                        entry.getValue() == null ? null : entry.getValue().toString());
+            }
+            return new RedoLogOffset(offsetStrMap);
+        }
+    }
+
+    /**
+     * The {@link ChangeEventSource.ChangeEventSourceContext} implementation for binlog split task.
+     */
+    private class BinlogSplitChangeEventSourceContext
+            implements ChangeEventSource.ChangeEventSourceContext {
+        @Override
+        public boolean isRunning() {
+            return taskRunning;
+        }
+    }
+}
